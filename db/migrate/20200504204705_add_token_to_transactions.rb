337147bf3e64feca1ac7822dcd60a29139943ae9
Crystal Mackey Free <cdmackeyfree@gmail.com>
2020-05-08 12:12:14 -0400

    updated schema to add token, implementing Express is a WIP

---
+++ db/migrate/20200504204705_add_token_to_transactions.rb
@@ -0,0 +1,5 @@
+class AddTokenToTransactions < ActiveRecord::Migration[5.0]
+  def change
+    add_column :transactions, :token, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200416153220) do
+ActiveRecord::Schema.define(version: 20200504204705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20200416153220) do
     t.datetime "updated_at",         null: false
     t.integer  "transaction_amount"
     t.integer  "ticket_amount"
+    t.string   "token"
   end
 
 end
